65b5813fe915b4c03f5dec7d896bcf3e132005e7
Roberto Oliveira <robertooliveira2604@gmail.com>
2015-10-12 18:10:42 -0300

    agora vai

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,7 @@ return array(
     'modules' => array(
         'DoctrineModule',
         'DoctrineORMModule',
-        'DoctrineDataFixtureModule',
+//        'DoctrineDataFixtureModule',
         'JMSSerializerModule',
         'Application',
         'SONUser',
